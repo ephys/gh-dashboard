82da8d46ca0d5dba6a66f83aeb8e3f716776f003
Zoé Cox <zoe@ephys.dev>
2024-04-22 17:14:40 +0200

    ignore tsc errors on build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,9 @@
+// eslint-disable-next-line spaced-comment
 /// <reference types="vite/client" />
+
+declare module 'csstype' {
+  interface Properties {
+    // Allow any CSS Custom Properties
+    [index: `--${string}`]: any;
+  }
+}
